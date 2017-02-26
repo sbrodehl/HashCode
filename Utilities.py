4377a028b471bc3654d80ad0e88e2001da6a60a1
Sebastian Brodehl <sbrodehl@students.uni-mainz.de>
2017-02-26 18:37:45 +0100

    Adding offline score computation.

--- Utilities.py
+++ Utilities.py
@@ -6,12 +6,13 @@ from tqdm import tqdm
 from collections import namedtuple
 
 
-def csv_print(mat):
-    np.savetxt(sys.stdout.buffer, mat, fmt='%.5f', newline="\n")
-
 Endpoint = namedtuple('Endpoint', ['id', 'lat', 'con'])
-Request  = namedtuple('Request', ['vid', 'eid', 'n'])
+Request = namedtuple('Request', ['vid', 'eid', 'n'])
 Scoring = namedtuple('Scoring', ['vid', 'cid', 'score'])
+
+
+def csv_print(mat, fmt='%.5f'):
+    np.savetxt(sys.stdout.buffer, mat, fmt=fmt, newline="\n")
 
 
 def sort_array_with_id(arr):
@@ -23,8 +24,27 @@ def sort_array_with_id(arr):
     tuple_arr = [(id, arr[id]) for id in range(len(arr))]
     return sorted(tuple_arr, key=lambda t: t[1])
 
-def check_vid_size(cachesize, videosizes):
-    for v in videosizes:
-        if cachesize < v:
-            print('moo')
 
+def compute_solution_score(videos_on_cache, requests, endpoints):
+    score = 0.0
+    total_req = 0
+
+    for req in requests:
+        # find caches that are connected to eid and have vid
+        _lats = [endpoints[req.eid].lat]
+
+        # check if video vid is on cache
+        for _ci, _c in enumerate(videos_on_cache):
+            if req.vid in _c:
+                # check if endpoint eid is connected to cache
+                for _cc in endpoints[req.eid].con:
+                    if _ci == _cc[0]:
+                        _lats.append(_cc[1])
+
+        min_lat = np.min(_lats)
+        diff_lat = endpoints[req.eid].lat - min_lat
+        saved_lad = req.n * diff_lat
+        score += saved_lad
+        total_req += req.n
+
+    return np.floor(score * 1000.0 / total_req)
