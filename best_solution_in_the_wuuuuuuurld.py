0e0045b1c738f3e9a89d04538a80215fbb3ff6df
Sebastian Brodehl <sbrodehl@students.uni-mainz.de>
2017-02-26 18:38:43 +0100

    New solution, needs more love.

--- best_solution_in_the_wuuuuuuurld.py
+++ best_solution_in_the_wuuuuuuurld.py
@@ -1,9 +1,7 @@
 import numpy as np
 from tqdm import tqdm
 from heapq import *
-from time import gmtime, strftime
-
-from pqdict import minpq
+from time import gmtime, strftime, sleep
 
 from IO import *
 
@@ -19,15 +17,105 @@ def cache_to_endpoint(n_cache, endpoints):
 
 
 def vid_to_endpoint(n_vid, n_end, requests):
-    res = np.zeros(shape=(n_vid, n_end), dtype=np.bool)
+    res = np.zeros(shape=(n_vid, n_end), dtype=np.uint32)
 
     for r in requests:
-        res[r.vid][r.eid] = True
+        res[r.vid][r.eid] = r.n
 
     return res
 
 
-def solution(n_vid, n_end, n_req, n_cache, s_cache, s_videos, endpoints, requests):
+def solution(graph):
+    # solutions
+    videos_on_cache = [[] for _ in range(graph['n_caches'])]
+    scores = {}
+
+    ScoreKey = namedtuple('ScoreKey', ['video', 'cache'])
+
+    for cid in tqdm(range(graph['n_caches']), desc="Computing Scores"):
+        scores_for_c = {}
+        cache = graph['caches'][cid]
+        # if there are requests, compute score
+        for eid in cache['endpoints']:
+            ep = graph['endpoints'][eid]
+            lat_diff = ep['latency']
+            for con in ep['connections']:
+                if con[0] == cid:
+                    lat_diff -= con[1]
+                    break
+
+            for rid in ep['requests']:
+                req = graph['requests'][rid]
+                key = ScoreKey(req.vid, cid)
+                score = 0.0
+                if key in scores_for_c:
+                    score = scores_for_c[key]
+                score -= req.n * lat_diff / graph['videos'][req.vid]['size']
+                scores_for_c[key] = score
+
+                del req
+                del key
+                del score
+
+            del ep
+
+        scores.update(scores_for_c)
+
+        del scores_for_c
+        del cache
+
+    pq = []
+    for k in scores:
+        heappush(pq, (scores[k], k.video, k.cache))
+
+    # now we have the heap with the scores
+    # print("Size of the heap", len(pq), "/", str(len(graph['videos']) * len(graph['caches'])))
+
+    # update from here on
+    pbar = tqdm(total=len(pq), desc="Progressing Priority Queue")
+    while pq:
+        pbar.update(1)
+        (s, v, c) = heappop(pq)
+        key = ScoreKey(v, c)
+        # check if score has changed
+        # push the tuple if score has changed
+        if scores[key] > s:
+            heappush(pq, (scores[key], v, c))
+
+        cache_size = np.sum([graph['videos'][vids]['size'] for vids in videos_on_cache[c]])
+        if cache_size + graph['videos'][v]['size'] > graph['max_cache_size']:
+            # continue if cache is full
+            continue
+
+        videos_on_cache[c].append(v)
+
+        # now update neighboring cache which need to hold the same video
+        cache = graph['caches'][c]
+        for eid in cache['endpoints']:
+            ep = graph['endpoints'][eid]
+            for nc in ep['connections']:
+                key = ScoreKey(v, nc)
+                if key in scores:
+                    lat_diff = ep['latency']
+                    for con in ep['connections']:
+                        if con[0] == nc:
+                            lat_diff -= con[1]
+                            break
+                    n_requests = 0
+                    for req in graph['videos'][v]['requests']:
+                        if req.eid == eid:
+                            n_requests = req.n
+                            break
+
+                    score = scores[key]
+                    score -= n_requests * lat_diff / graph['videos'][v]['size']
+                    scores[key] = score
+                    pass
+
+    return videos_on_cache
+
+
+def solution_old(n_vid, n_end, n_req, n_cache, s_cache, s_videos, endpoints, requests):
     # solutions
     cache = np.zeros(n_cache)
     videos_on_cache = [[] for _ in range(n_cache)]
@@ -35,68 +123,66 @@ def solution(n_vid, n_end, n_req, n_cache, s_cache, s_videos, endpoints, request
     scores = np.zeros(shape=(n_vid, n_cache), dtype=np.double)
     in_q = np.zeros(shape=(n_vid, n_cache), dtype=np.double)
 
-    pq = minpq()
+    pq = []
 
     c2e = cache_to_endpoint(n_cache, endpoints)
     v2e = vid_to_endpoint(n_vid, n_end, requests)
 
-    for req in tqdm(requests):
+    for req in tqdm(requests, desc="Computing scores for Video/Cache Pairs"):
         ep = endpoints[req.eid]
         for c in ep.con:
             d_latency = ep.lat - c[1]
             score = req.n * d_latency * (1.0 / s_videos[req.vid])
             scores[req.vid][c[0]] -= score
 
-    for req in tqdm(requests):
+    for req in tqdm(requests, desc="Pushing Video/Cache Pairs into Priority Queue"):
         ep = endpoints[req.eid]
         for c in ep.con:
             if not in_q[req.vid][c[0]]:
                 in_q[req.vid][c[0]] = True
-                # heappush(pq, (scores[req.vid][c[0]], req.vid, c[0]))
-                index = req.vid * n_cache + c[0]
-                pq[index] = scores[req.vid][c[0]]
+                heappush(pq, (scores[req.vid][c[0]], req.vid, c[0]))
 
-    print((len(pq))/(n_vid * n_cache), "in Queue.")
-
+    tqdm.write(str(100 * (len(pq))/(n_vid * n_cache)) + "% in Queue.")
+    sleep(0.1)
+    pbar = tqdm(total=len(pq), desc="Progressing Priority Queue")
     # update from here on
     while pq:
-        # (s, v, c) = heappop(pq)
-        key, s = pq.popitem()
-        v, c = key // n_cache, key % n_cache
+        pbar.update(1)
+        (s, v, c) = heappop(pq)
+
+        # check if score has changed
+        if any(v in cache_ for cache_ in videos_on_cache):
+            if scores[v][c] > s:
+                heappush(pq, (scores[v][c], v, c))
+
         if cache[c] + s_videos[v] <= s_cache:
             videos_on_cache[c].append(v)
             cache[c] += s_videos[v]
 
-            print(len(pq))
+            if not True:
+                # update scores for connected caches / videos
+                for req in requests:
+                    if req.vid == v:
+                        ep = endpoints[req.eid]
+                        for ca in ep.con:
+                            d_latency = ep.lat - ca[1]
+                            score = v2e[v][req.eid] * d_latency * (1.0 / s_videos[v])
+                            # is this the right one?
+                            scores[v][ca[0]] = 0  # score
+            else:
+                # update scores for connected caches / videos
+                for eid in range(n_end):
+                    # endpoint connects to cache
+                    if c2e[c][eid]:
+                        # if video is requested by the endpoint
+                        if v2e[v][eid]:
+                            ep = endpoints[eid]
+                            for ec in ep.con:
+                                d_latency = ep.lat - ec[1]
+                                score = v2e[v][eid] * d_latency * (1.0 / s_videos[v])
+                                scores[v][ec[0]] = 0  # score
 
-            # update scores for connected caches / videos
-            # for req in requests:
-            #     if req.vid == v:
-            #         ep = endpoints[req.eid]
-            #         for ca in ep.con:
-            #             d_latency = ep.lat - ca[1]
-            #             score = req.n * d_latency * (1.0 / s_videos[req.vid])
-            #             scores[req.vid][ca[0]] = 0  # score
-
-            # update scores for connected caches / videos
-            for eid in range(n_end):
-                # endpoint connects to cache
-                if c2e[c][eid]:
-                    # if video is requested by the endpoint
-                    if v2e[v][eid]:
-                        for cc in endpoints[eid].con:
-                            scores[v][cc[0]] = 0
-
-            # update the pq
-            for (k, ss) in pq.items():
-                vv, cc = k // n_cache, k % n_cache
-                new_score = scores[vv][cc]
-                if ss != new_score:
-                    pq[k] = new_score
-                # (_, vvvv, cccc) = heappop(pq)
-                # heappush(pq, (scores[vvvv][cccc], vvvv, cccc))
-
-        if len(pq) % 100000 == 0:
+        if len(pq) % 1000 == 0:
             write_solution(strftime("%Y%m%d-%H%M%S", gmtime()), cache, videos_on_cache)
 
     return cache, videos_on_cache
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import argparse
 from tqdm import tqdm
+import time
+from time import sleep
 
 from Utilities import *
 from IO import *
@@ -15,9 +17,20 @@ parser.add_argument("output", help="output file")
 args = parser.parse_args()
 
 print(args)
+start = time.time()
 
 n_vid, n_end, n_req, n_cache, s_cache, s_videos, endpoints, requests = read_dataset(args.input)
+graph = build_graph(n_vid, n_end, n_req, n_cache, s_cache, s_videos, endpoints, requests)
 
-cache, videos_on_cache = solution(n_vid, n_end, n_req, n_cache, s_cache, s_videos, endpoints, requests)
+tqdm.write("Setup in {0:.2f}s".format((time.time() - start)))
+sleep(0.1)
 
-write_solution(args.output, cache, videos_on_cache)
+videos_on_cache = solution(graph)
+# cache, videos_on_cache = solution(n_vid, n_end, n_req, n_cache, s_cache, s_videos, endpoints, requests)
+
+# write_solution(args.output, cache, videos_on_cache)
+
+end = time.time()
+sleep(0.1)
+score = compute_solution_score(videos_on_cache, requests, endpoints)
+print("Score {0} in {1:.2f}s".format(score, (end - start)))
