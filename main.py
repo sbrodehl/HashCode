6203e185179e60bcd36be0651b873a4819da04b2
Sebastian Brodehl <sbrodehl@students.uni-mainz.de>
2017-03-01 21:14:53 +0100

    Don’t measure file IO.

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ parser.add_argument("input", help="input file")
 parser.add_argument("output", help="output file")
 args = parser.parse_args()
 
+n_vid, n_end, n_req, n_cache, s_cache, s_videos, endpoints, requests = read_dataset(args.input)
+
 start = time.time()
 
-n_vid, n_end, n_req, n_cache, s_cache, s_videos, endpoints, requests = read_dataset(args.input)
 graph = build_graph(n_vid, n_end, n_req, n_cache, s_cache, s_videos, endpoints, requests)
 graph2, uses_clusters = preprocessing(graph)
 
@@ -24,9 +25,9 @@ videos_on_cache = solution(graph2)
 if uses_clusters:
     videos_on_cache = postprocessing(videos_on_cache, graph2['cache_mapping'], s_cache, s_videos)
 
-write_solution(args.output, videos_on_cache)
-
 end = time.time()
+
+write_solution(args.output, videos_on_cache)
 
 score = compute_solution_score(videos_on_cache, requests, endpoints)
 tqdm.write("Score {0:.0f} in {1:.2f}s".format(score, (end - start)))
