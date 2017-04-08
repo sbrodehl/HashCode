61d4928cb65a7f318bb09f0b5d31ad4fde2e6f20
Sebastian Brodehl <sbrodehl@students.uni-mainz.de>
2017-04-08 23:25:12 +0200

    Removing unused method.

--- Final Round/IO.py
+++ Final Round/IO.py
@@ -27,6 +27,7 @@ def read_dataset(fpath):
         return {'H': H, 'W': W, 'R': R, 'Pb': Pb, 'Pr': Pr, 'B': B,
                 'backbone': backbone, 'graph': matrix}
 
+
 def write_solution(filepath, videos_on_cache):
     used_caches = 0
     for cache in videos_on_cache:
@@ -41,52 +42,6 @@ def write_solution(filepath, videos_on_cache):
                 out = str(idx) + " " + " ".join(str(i) for i in c)
                 f.write(out)
                 f.write('\n')
-
-
-def build_graph(n_vid, n_end, n_req, n_cache, s_cache, s_videos, endpoints, requests):
-    graph = {
-        'n_videos': n_vid,
-        'n_endpoints': n_end,
-        'n_requests': n_req,
-        'n_caches': n_cache,
-        'max_cache_size': s_cache,
-        'requests': requests,
-        'caches': [{} for _ in range(n_cache)],
-        'videos': [{} for _ in range(n_vid)],
-        'endpoints': [{} for _ in range(n_end)]
-    }
-
-    tqdm.write("Setting up Graph")
-
-    # insert caches with corresponding endpoints
-    for c in tqdm(range(n_cache), desc="Caches"):
-        graph['caches'][c] = {
-            'endpoints': [],
-            'size': s_cache
-        }
-
-    # insert endpoints with corresponding requests
-    for e in tqdm(range(n_end), desc="Caches + Endpoints"):
-        graph['endpoints'][e] = {
-            'latency': endpoints[e].lat,
-            'connections': endpoints[e].con,
-            'requests': []
-        }
-        for con in endpoints[e].con:
-            graph['caches'][con[0]]['endpoints'].append(e)
-
-    # insert videos with corresponding requests
-    for v in tqdm(range(n_vid), desc="Videos"):
-        graph['videos'][v] = {
-            'size': s_videos[v],
-            'requests': []
-        }
-
-    for i, r in tqdm(enumerate(requests), desc="Inserting Requests"):
-        graph['videos'][r.vid]['requests'].append(i)
-        graph['endpoints'][r.eid]['requests'].append(i)
-
-    return graph
 
 
 if __name__ == '__main__':
