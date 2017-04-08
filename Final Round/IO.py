724219700b85c81d19df3b211e01bbc396c60f8c
Alexej Disterhoft <disterhoft@uni-mainz.de>
2017-04-08 23:46:44 +0200

    write solution.

--- Final Round/IO.py
+++ Final Round/IO.py
@@ -36,10 +36,36 @@ def read_dataset(fpath):
         }
 
 
-def write_solution(filepath, d):
-    pass
+def write_solution(fpath, D):
+    # look for cables and routers, find positions and write that stuff
+    cables = []
+    routers = []
+
+    graph = D['graph']
+    for x, row in enumerate(graph):
+        for y, val in enumerate(row):
+            if val == 2:
+                cables.append((x,y))
+            elif val == 3:
+                routers.append((x,y))
+
+    with open(fpath, 'w') as writer:
+        writer.write("%d\n" % len(cables))
+        for cable in cables:
+            writer.write("%d %d\n" % (cable[0], cable[1]))
+
+        writer.write("%d\n" % len(routers))
+        for router in routers:
+            writer.write("%d %d\n" % (router[0], router[1]))
+
+        writer.close()
 
 if __name__ == '__main__':
     import sys
     fpath = sys.argv[1]
-    print(read_dataset(fpath))
+    D = read_dataset(fpath)
+    bb = D['backbone']
+    D['graph'][bb[0]+1, bb[1]] = 3
+    write_solution(sys.argv[2], D)
+
+
