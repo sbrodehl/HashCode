e2cb6d622a90d4cc0849fb5c425f544a3a0eccaa
Sebastian Brodehl <sbrodehl@students.uni-mainz.de>
2017-04-09 18:33:28 +0200

    Finalising score function.

--- Final Round/Utilities.py
+++ Final Round/Utilities.py
@@ -1,49 +1,39 @@
 import sys
 import numpy as np
-from tqdm import tqdm
 
-from collections import namedtuple
-
-
-Endpoint = namedtuple('Endpoint', ['id', 'lat', 'con'])
-Request = namedtuple('Request', ['vid', 'eid', 'n'])
-Scoring = namedtuple('Scoring', ['vid', 'cid', 'score'])
+from IO import Cell
+from best_solution_in_the_wuuuuuuurld import wireless_access
 
 
 def csv_print(mat, fmt='%.5f'):
     np.savetxt(sys.stdout.buffer, mat, fmt=fmt, newline="\n")
 
 
-def sort_array_with_id(arr):
-    """
-    Sort array ascending and keep track of ids.
-    :param arr: array with values
-    :return: array with tuples (id, val)
-    """
-    tuple_arr = [(id, arr[id]) for id in range(len(arr))]
-    return sorted(tuple_arr, key=lambda t: t[1])
-
-
 def compute_solution_score(d):
-    score = 0.0
-
-    # look for cables and routers, find positions and write that stuff
+    # look for cables and routers, compute coverage and cost
     cables = []
     routers = []
 
-    graph = d['graph']
-    for x, row in enumerate(graph):
+    g = d['graph']
+    for x, row in enumerate(g):
         for y, val in enumerate(row):
-            if val == 2:
+            if val == Cell.Cable:
                 cables.append((x, y))
-            elif val == 3:
+            elif val == Cell.ConnectedRouter:
                 routers.append((x, y))
                 cables.append((x, y))
 
     # budget part of scoring
     score_cost = d['budget'] - (len(cables) * d['price_backbone'] + len(routers) * d['price_router'])
-
-    # the other part
-    # todo.
-
+    coverage = (d['original'] == Cell.Wireless).astype(np.int8)
+    for (a, b) in routers:
+        mask = wireless_access(a, b, d)
+        for x, row in enumerate(mask):
+            for y, val in enumerate(row):
+                if mask[x][y]:
+                    w = x + a - d['radius']
+                    v = y + b - d['radius']
+                    coverage[w][v] = 2
+    coverage = (coverage == 2).astype(np.int8)
+    score = 1000 * np.sum(coverage)
     return np.floor(score + score_cost)
