7872695230e0f86c27f390bd66e7045288022632
Sebastian Brodehl <sbrodehl@students.uni-mainz.de>
2017-04-09 11:30:24 +0200

    Returning mask with wireless access for given router position in graph.

--- Final Round/best_solution_in_the_wuuuuuuurld.py
+++ Final Round/best_solution_in_the_wuuuuuuurld.py
@@ -14,33 +14,48 @@ def unit_circle_vectorized(r):
     return ret.astype(int)
 
 
-def wireless_access(h, w, d):
-    mask = np.zeros((d["height"], d["width"]))
+def wireless_access(a, b, d):
     g = d["graph"]
     r = d["radius"]
     circle = unit_circle_vectorized(r)
+    mask = circle
     for w_off in range(-r, r):
         for h_off in range(-r, r):
             if not circle[h_off + r][w_off + r]:
                 continue
-            h_coord = h + h_off
-            w_coord = w + w_off
-            if h_coord not in range(0, d["height"]):
+            if h_off == 0 and w_off == 0:
                 continue
-            if w_coord not in range(0, d["width"]):
+            x = a + h_off
+            y = b + w_off
+            if x not in range(0, d["height"]):
                 continue
-            field = g[h_coord][w_coord]
-            print(field)
-            # construct smallest enclosing rectangle
+            if y not in range(0, d["width"]):
+                continue
+            field = g[x][y]
+            if not field == 1:
+                # set others fields to zero
+                mask[h_off + r][w_off + r] = 0
+                continue
 
-    ho = int((r + 2 - 1) / 2)  # SDIV
-    h_from = np.max([0, h - ho])
-    h_to = np.min([d["height"] - 1, h + ho])
-    w_from = np.max([0, w - ho])
-    w_to = np.min([d["width"] - 1, w + ho])
-    rows = range(h_from, h_to)
-    cols = range(w_from, w_to)
-    crop = d["graph"][rows][:, cols]
+            # construct smallest enclosing rectangle
+            r_min = np.min([a, x])
+            rows = range(r_min, np.max([a, x]) + 1)
+            c_min = np.min([b, y])
+            cols = range(c_min, np.max([b, y]) + 1)
+            # loop over rectangle and check condition
+            # TODO if there is at least one wall cell in closing rectangle condition is true?
+            for w in rows:
+                for v in cols:
+                    ftype = g[w][v]
+                    # if this is a wall cell
+                    if not ftype == 0:
+                        continue
+                    # check if wall is in 'sight'
+                    if np.min([a, x]) <= w <= np.max([a, x]) and np.min([b, y]) <= v <= np.max([b, y]):
+                        # ALARM!
+                        mask[h_off + r][w_off + r] = 0
+                        # TODO some early stopping?
+
     return mask
 
 
@@ -99,7 +114,7 @@ def solution(d):
     skeleton = skeletonize(wireless)
     med_axis = medial_axis(wireless)
 
-    mask = wireless_access(0, 0, d)
+    mask = wireless_access(107, 42, d)
 
     # display results
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4),
